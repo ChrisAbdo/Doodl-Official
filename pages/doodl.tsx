9bca88dce2d454a49ba598477cfa4b4f91626b31
chris abdo <chris.j.abdo@gmail.com>
2022-11-22 13:38:44 -0500

    ADDED: Alert on drawing page

--- pages/doodl.tsx
+++ pages/doodl.tsx
@@ -268,6 +268,32 @@ const Page: FC<pageProps> = ({}) => {
 
   return (
     <>
+      <div className="alert alert-info shadow-lg mb-8">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-current flex-shrink-0 w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <h1>Need funds? Visit the Mumbai Faucet! </h1>
+          <a
+            href="https://mumbaifaucet.com"
+            target="_blank"
+            rel="noreferrer"
+            className=" hover:text-gray-600"
+          >
+            https://mumbaifaucet.com
+          </a>
+        </div>
+      </div>
       <div className="flex items-center text-center justify-center">
         <div className="card rounded-none w-96 bg-base-100 border-[2px] border-black text-center items-center">
           <div className="card-body text-center  justify-center items-center">
